faec4ffaca87b30bba1403092ae360d92770ed36
Vincent Girard <vGIRARD@juniorisep.com>
2024-04-16 12:18:11 +0200

    mosquitto code de base

--- mosquitto.py
+++ mosquitto.py
@@ -1,37 +1,47 @@
 import paho.mqtt.client as mqtt
 import turtle
+import json
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "vgi")
-'#client.connect("srv-lora.isep.fr")'
-client.connect("broker.hivemq.com")
+client.connect("srv-lora.isep.fr")
+#client.connect("broker.hivemq.com")
+
+
+screen = turtle.Screen()
+screen.setup(800, 600)
+screen.title("Positions des objets")
+
+pen = turtle.Turtle()
+pen.penup()
+
 
 
 def on_message_callback(client_inst, userdata, message):
-    valeur = message.payload.decode("ansi")
-    print(message.topic + " " + valeur)
+    try:
+        valeur = message.payload.decode("ansi")
+        print(message.topic + " " + valeur)
+        valeur_json = json.loads(valeur)
+        valeur_lora = valeur_json["object"]["message"]
+        liste = valeur_lora.split(":")
+        nom = liste[0]
+        position = (int(liste[1]), int(liste[2]))
+        dessiner_point(position)
+        print("Nom:", nom)
+        print("Position:", position)
+    except Exception as e:
+        print("Erreur lors de la lecture du message JSON:", e)
 
-    if valeur == "fin":
-        turtle.bye()
-        exit()
-    elif valeur[0] == "p":
-        try:
-            xy = valeur.split(":")
-            turtle.goto(int(xy[1]), int(xy[2]))
-        except Exception as e:
-            print(f"Erreur de position - {e}")
-    elif valeur[0] == "c":
-        try:
-            couleur = valeur.split(":")[1]
-            turtle.color(couleur)
-        except Exception as e:
-            print(f"Erreur de couleur - {e}")
-    else:
-        print("Erreur : commande inconnue...")
 
+
+def dessiner_point(position, couleur="red"):
+    pen.pendown()
+    pen.goto(position)
+    pen.dot(5, couleur)
+    pen.penup()
 
 client.on_message = on_message_callback
 
-client.subscribe("hal/val")
+client.subscribe("#")
 client.loop_start()
 
 turtle.mainloop()
